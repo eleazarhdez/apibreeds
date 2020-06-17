995382c24c2ef9689ba876e6481e4486cf24ae51
Eleazar <eleazar87@gmail.com>
2020-06-17 16:04:59 +0100

    Create getAllBreeds function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const config = require('./config')
 const app = express();
+const service = require('./service')
+
+app.use('/api/', service);
 
 app.listen(config.api.port, () => {
   console.log(`API listening on port ${config.api.port}`);
